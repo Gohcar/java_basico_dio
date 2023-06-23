c84ad959bfcf6f0a6c6b3b8478ad29b8124de422
Gohcar <glauberrrocha@gmail.com>
2023-06-23 16:44:58 -0300

    processo seletivo

--- projeto_java_dio/src/edu/glauber/primeirasemana/Problem.java
+++ projeto_java_dio/src/edu/glauber/primeirasemana/Problem.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Problem {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-// TODO: complete os espaços em branco com sua solução para o problema
+
 		String[] nomes = new String[   10  ];
 		
 		for(int i=0 ; i<10 ; i++){
